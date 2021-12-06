abb0140add1bbe5ed9db968c59356efba616368c
Nycole Xavier <nycole.xavier@hotmail.com>
2021-12-05 20:29:10 -0400

    paramentrização nas rotas

---
+++ src/pages/api/users/[id].tsx
@@ -0,0 +1,13 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    const id = request.query
+
+    const users = [
+        { i: 1, name: 'Diego'},
+        { i: 2, name: 'Dani'},
+        { i: 3, name: 'Rafa'},
+    ]
+
+    return response.json(users)
+}
