5f49626fd420fa310af1f7e8fd8272b46627c026
Nycole Xavier <nycole.xavier@hotmail.com>
2021-12-16 13:02:18 -0400

    commit teste

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,11 +4,9 @@ import Providers from 'next-auth/providers'
 export default NextAuth ({
 
     providers: [
-            Providers.Github({
-                clientId: process.env.GITHUB_ID,
-                clientSecret: process.env.GITHUB_SECRET
-            }),
+        Providers.GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+        }),
     ],
-
-    database: process.env.DATABASE_URL,
-})git 
+})
